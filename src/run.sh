23e17c207ccbbad8bf3fdbee623d4d732513cded
locw <lewysward99@gmail.com>
2020-03-05 13:15:42 +0000

    Removed savile row directory from repo

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,3 @@
-savilerow-1.8.0-linux/savilerow lbs.eprime parameters/SolvableInstances/$1 -run-solver
+../savilerow-1.8.0-linux/savilerow lbs.eprime parameters/SolvableInstances/$1 -run-solver
 
 cat parameters/SolvableInstances/*.solution
--- src/savilerow-1.8.0-linux/examples/blackHole/mkparameters.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import random
-
-myarr=list(range(1,52))
-
-random.shuffle(myarr)
-assert len(myarr)==51
-print("letting initialStacks = "+str(myarr))
