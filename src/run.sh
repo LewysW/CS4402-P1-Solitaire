4fd7c17c5545b3f30a2f5556fa089a5daf25761e
locw <lewysward99@gmail.com>
2020-03-05 15:04:27 +0000

    Specification currently finds a card that is in a specified suit and in the list of cards

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,6 @@
 ../savilerow-1.8.0-linux/savilerow lbs.eprime parameters/SolvableInstances/$1 -run-solver
 
 cat parameters/SolvableInstances/*.solution
+
+rm parameters/SolvableInstances/*.solution
+
